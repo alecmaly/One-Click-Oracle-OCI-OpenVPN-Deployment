b324a38d442e8df707fbf734dc8a68be0f7b4de8
Alec Maly <alecjmaly@gmail.com>
2022-11-27 15:06:45 -0500

    Update new_oci_openvpn_server.sh

--- new_oci_openvpn_server.sh
+++ new_oci_openvpn_server.sh
@@ -148,6 +148,10 @@ ocid_instance=$(oci compute instance launch --display-name "${instance_name}" --
 --hostname-label "$instance_name" \
 --raw-output)
 
+# unlikely failure tmp fix, should loop ssh cmd instead until alive
+echo "[+] VM awake, sleeping an extra 10 seconds to wake up SSH."
+sleep 10
+
 echo "[+] Getting Public IP"
 public_ip=`oci compute instance list-vnics --instance-id $ocid_instance | jq -r '.data[]."public-ip"'`
 
